26da93b727493c0478b5b91bb195df6ba2be1ff4
Taavi Burns <taavi@pagerduty.com>
2018-12-05 08:05:10 -0500

    5-optimize

--- 2018/5/5b-async_stream.exs
+++ 2018/5/5b-async_stream.exs
@@ -1,6 +1,6 @@
 defmodule P do
   def react(s) do
-    s2 = react(s |> String.to_charlist, []) |> String.Chars.to_string
+    s2 = s |> react([]) |> react([])
     if s == s2 do
       s
     else
@@ -8,7 +8,7 @@ defmodule P do
     end
   end
 
-  defp react([], acc), do: Enum.reverse(acc)
+  defp react([], acc), do: acc
   for {lower, upper} <- Enum.zip(?a..?z, ?A..?Z) do
     defp react([unquote(lower), unquote(upper) | rest], acc), do: react(rest, acc)
     defp react([unquote(upper), unquote(lower) | rest], acc), do: react(rest, acc)
@@ -18,14 +18,16 @@ end
 
 File.read!("input.txt")
 |> String.trim
+|> String.to_charlist
 |> (fn s ->
   Enum.zip(?a..?z, ?A..?Z)
-  |> Enum.map(fn {lower, upper} -> s |> String.replace(<<lower>>, "") |> String.replace(<<upper>>, "") end)
+  |> Enum.map(fn {lower, upper} -> s |> Enum.filter(fn c -> c != lower && c != upper end) end)
 end).()
 |> Task.async_stream(&P.react/1)
-|> Enum.map(fn {:ok, s} -> byte_size(s) end)
+|> Enum.map(fn {:ok, s} -> length(s) end)
 |> Enum.min
 |> IO.puts
 
 # 4572
 # takes as little as 3s!
+# 2s when optimized
