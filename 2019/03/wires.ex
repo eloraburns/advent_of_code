b1c431d5e2c3d616c7345d72b7f7b54359e5020e
Taavi Burns <taavi@pagerduty.com>
2019-12-03 20:11:08 -0500

    2019-3b

--- 2019/03/wires.ex
+++ 2019/03/wires.ex
@@ -14,31 +14,46 @@ defmodule Wires do
     |> Enum.flat_map_reduce({0, 0}, fn {dir, len}, {cx, cy} ->
       case dir do
         ?U -> {
-          Enum.map(cy..(cy+len), &({cx, &1})),
+          Enum.map((cy+1)..(cy+len), &({cx, &1})),
           {cx, cy+len}
         }
         ?D -> {
-          Enum.map(cy..(cy-len), &({cx, &1})),
+          Enum.map((cy-1)..(cy-len), &({cx, &1})),
           {cx, cy-len}
         }
         ?L -> {
-          Enum.map(cx..(cx-len), &({&1, cy})),
+          Enum.map((cx-1)..(cx-len), &({&1, cy})),
           {cx-len, cy}
         }
         ?R -> {
-          Enum.map(cx..(cx+len), &({&1, cy})),
+          Enum.map((cx+1)..(cx+len), &({&1, cy})),
           {cx+len, cy}
         }
       end
     end)
     |> elem(0)
-    |> tl
+  end
+
+  def index_of({x, y}, wire) do
+    wire
+    |> Enum.with_index(1)
+    |> Enum.find(fn {{wx, wy}, _i} -> wx == x and wy == y end)
+    |> elem(1)
   end
 
   def solve_1a(filename \\ "input.txt") do
     [w1, w2] = load!(filename) |> Enum.map(&wire_to_path/1) |> Enum.map(&MapSet.new/1)
     MapSet.intersection(w1, w2)
     |> Enum.map(fn {x, y} -> abs(x) + abs(y) end)
+    |> Enum.min
+  end
+
+  def solve_1b(filename \\ "input.txt") do
+    [w1, w2] = load!(filename) |> Enum.map(&wire_to_path/1)
+    MapSet.intersection(MapSet.new(w1), MapSet.new(w2))
+    |> Enum.map(fn coord ->
+      index_of(coord, w1) + index_of(coord, w2)
+    end)
     |> Enum.min
   end
 end
