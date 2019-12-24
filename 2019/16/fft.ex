39972e13e48d7d3670785826bddffcc00a93195d
Taavi Burns <taavi@pagerduty.com>
2019-12-24 04:11:26 -0500

    2019-12-16b

--- 2019/16/fft.ex
+++ 2019/16/fft.ex
@@ -21,6 +21,18 @@ defmodule FFT do
     |> Enum.concat
   end
 
+  def coefficient(in_digit, out_digit) do
+    (in_digit + 1)
+    |> div(out_digit + 1)
+    |> rem(4)
+    |> case do
+      0 -> 0
+      1 -> 1
+      2 -> 0
+      3 -> -1
+    end
+  end
+
   def step(data, coeffs) do
     data
     |> Stream.cycle
@@ -46,5 +58,33 @@ defmodule FFT do
     |> Enum.map(&to_string/1)
     |> Enum.join("")
     # 73127523...in 26s
+  end
+
+  def pv2int(l, acc \\ 0)
+  def pv2int([], acc), do: acc
+  def pv2int([h | t], acc), do: pv2int(t, (acc * 10) + h)
+
+  def faststep(ints), do: faststep(Enum.reverse(ints), 0, [])
+  def faststep([], _, acc), do: acc
+  def faststep([d | rest], sum, acc) do
+    newsum = sum + d
+    newdig = newsum |> rem(10) |> abs
+    faststep(rest, newsum, [ newdig | acc ])
+  end
+
+  def solve_1b(filename \\ "input.txt") do
+    input = load!(filename)
+    offset = input |> Enum.take(7) |> pv2int
+    full_input = Enum.flat_map(1..10000, fn _ -> input end) |> Enum.drop(offset)
+    # Make sure that this optimization works
+    true = length(full_input) < offset
+    Stream.iterate(full_input, fn s -> IO.inspect(:timer.tc fn -> faststep(s) end) |> elem(1) end)
+    |> Stream.drop(100)
+    |> Enum.take(1)
+    |> hd
+    |> Enum.take(8)
+    |> Enum.map(&to_string/1)
+    |> Enum.join("")
+    # 80284420
   end
 end
