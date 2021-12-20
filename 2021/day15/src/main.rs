a730a48e369e65cb37f942cfbafcc34935228e21
Taavi Burns <taavi@taaviburns.ca>
2021-12-20 00:52:27 -0500

    2021-12-15 in rust

--- 2021/day15/src/main.rs
+++ 2021/day15/src/main.rs
@@ -1,125 +1,155 @@
+use std::collections::HashSet;
+use std::cmp::Reverse;
+
 const ZERO: u8 = 48;
 
 fn main() {
-    println!("15a(test), expect 40: {}", solvea("test.txt"));
+    println!("15a(test), expect 40: {}", solve(parsea("test.txt")));
+    println!("15b(test), expect 315: {}", solve(parseb("test.txt")));
 
     // 697 is too high
-    println!("15a(input): {}", solvea("input.txt"));
+    // 696 is right!
+    println!("15a(input): {}", solve(parsea("input.txt")));
+    println!("15b(input): {}", solve(parseb("input.txt")));
 }
 
-fn parse(filename: &str) -> Vec<Vec<usize>> {
+fn parsea(filename: &str) -> Vec<Vec<usize>> {
     std::fs::read_to_string(filename)
         .unwrap()
         .lines()
         .map(|l| {
             l.bytes().map(|b| (b - ZERO) as usize).collect()
-        } )
+        })
         .collect()
 }
 
-#[derive(Copy,Clone)]
-struct HeapNode {
+fn parseb(filename: &str) -> Vec<Vec<usize>> {
+    let smallcave = parsea(filename);
+    let smallwidth = smallcave.first().unwrap().len();
+    let smallheight = smallcave.len();
+    (0..(smallheight * 5))
+        .map(|i| i / smallheight)
+        .zip(smallcave.iter().cycle())
+        .map(|(i, row)| {
+            (0..(smallwidth * 5))
+                .map(|j| j / smallwidth)
+                .zip(row.iter().cycle())
+                .map(|(j, c)| {
+                    let new_c = i + j + c;
+                    if new_c > 9 {
+                        new_c - 9
+                    } else {
+                        new_c
+                    }
+                })
+                .collect()
+        })
+        .collect()
+}
+
+struct SeenNode {
+    cost: usize,
     x: usize,
     y: usize,
-    cost: usize,
-}
-type Heap = Vec<HeapNode>;
-
-fn heap_new() -> Heap {
-    vec![]
 }
 
-fn heap_push(heap: &mut Heap, node: HeapNode) {
-    let i = heap.len();
-    heap.push(node);
-    heap_bub_up(heap, i);
-}
-
-fn heap_bub_up(heap: &mut Heap, index: usize) {
-    let mut i = index;
-    while i > 0 {
-        let parent = i >> 1;
-        if heap[parent].cost > heap[i].cost {
-            heap_swap(heap, parent, i);
-        }
-        i = parent;
-    }
-}
-
-fn heap_bub_down(heap: &mut Heap, index: usize) {
-    let mut i = index;
-    loop {
-        let l = i << 1;
-        if l >= heap_len(heap) {
-            break;
-        }
-        let r = l + 1;
-        if r >= heap_len(heap) {
-            // handle l only
-            if heap[i].cost > heap[l].cost {
-                heap_swap(heap, i, l);
-            }
-            // Because there was only an l leaf, we are definitely at the bottom,
-            // so immediately break, we're done here.
-            break;
-        } else {
-            // handle l and r
-            if heap[l].cost < heap[r].cost {
-                if heap[l].cost < heap[i].cost {
-                    heap_swap(heap, i, l);
-                }
-                i = l;
-            } else {
-                if heap[r].cost < heap[i].cost {
-                    heap_swap(heap, i, r);
-                }
-                i = r;
-            }
-        }
-    }
-}
-
-fn heap_len(heap: &Heap) -> usize {
-    heap.len()
-}
-
-fn heap_swap(heap: &mut Heap, i: usize, j: usize) {
-    let t = heap[i];
-    heap[i] = heap[j];
-    heap[j] = t;
-}
-
-fn heap_pop(heap: &mut Heap) -> HeapNode {
-    let retval = heap[0];
-    heap[0] = heap.pop().unwrap();
-    heap_bub_down(heap, 0);
-    retval
-}
-
-fn heap_update(heap: &mut Heap, node: &HeapNode) {
-    for i in 0..heap.len() {
-        if node.x == heap[i].x && node.y == heap[i].y {
-            heap[i].cost = node.cost;
-            if i > 0 && heap[i >> 1].cost > heap[i].cost {
-                heap_bub_up(heap, i);
-            } else {
-                heap_bub_down(heap, i);
-            }
-            break;
-        }
-    }
-}
-
-fn solvea(filename: &str) -> usize {
-    let cave = parse(filename);
+fn solve(cave: Vec<Vec<usize>>) -> usize {
     let width = cave.first().unwrap().len();
     let height = cave.len();
+    let end_x = width - 1;
+    let end_y = height - 1;
 
-    let mut unseen = heap_new();
-    let mut sssp = vec![vec![width*height*10 as usize; width]; height];
-    sssp[0][0] = 0;
+    let mut horizon: Vec<SeenNode> = vec![SeenNode { cost: 0, x: 0, y: 0 }];
+    let mut donezo: HashSet<(usize, usize)> = HashSet::new();
+    let mut pathcosts: Vec<Vec<usize>> = vec![vec![usize::MAX; width]; height];
+    pathcosts[0][0] = 0;
+    // print_pathcosts(&cave);
+    loop {
+        // print_pathcosts(&pathcosts);
+        horizon.sort_by_key(|a| Reverse(a.cost));
+        let here = horizon.pop().unwrap();
+        let x = here.x;
+        let y = here.y;
+        if x == end_x && y == end_y {
+            return here.cost;
+        }
+        if x > 0 && !donezo.contains(&(x-1, y)) && (here.cost + cave[y][x-1]) < pathcosts[y][x-1] {
+            let new_pathcost = here.cost + cave[y][x-1];
+            pathcosts[y][x-1] = new_pathcost;
+            let mut i = usize::MAX;
+            for (si, s) in horizon.iter().enumerate() {
+                if s.x == x-1 && s.y == y {
+                    i = si;
+                    break;
+                } 
+            }
+            if i < usize::MAX {
+                horizon[i].cost = new_pathcost;
+            } else {
+                horizon.push(SeenNode { cost: new_pathcost, x: x-1, y: y });
+            }
+        }
+        if x < end_x && !donezo.contains(&(x+1, y)) && (here.cost + cave[y][x+1]) < pathcosts[y][x+1] {
+            let new_pathcost = here.cost + cave[y][x+1];
+            pathcosts[y][x+1] = new_pathcost;
+            let mut i = usize::MAX;
+            for (si, s) in horizon.iter().enumerate() {
+                if s.x == x+1 && s.y == y {
+                    i = si;
+                    break;
+                } 
+            }
+            if i < usize::MAX {
+                horizon[i].cost = new_pathcost;
+            } else {
+                horizon.push(SeenNode { cost: new_pathcost, x: x+1, y: y });
+            }
+        }
+        if y > 0 && !donezo.contains(&(x, y-1)) && (here.cost + cave[y-1][x]) < pathcosts[y-1][x] {
+            let new_pathcost = here.cost + cave[y-1][x];
+            pathcosts[y-1][x] = new_pathcost;
+            let mut i = usize::MAX;
+            for (si, s) in horizon.iter().enumerate() {
+                if s.x == x && s.y == y-1 {
+                    i = si;
+                    break;
+                } 
+            }
+            if i < usize::MAX {
+                horizon[i].cost = new_pathcost;
+            } else {
+                horizon.push(SeenNode { cost: new_pathcost, x: x, y: y-1 });
+            }
+        }
+        if y < end_y && !donezo.contains(&(x, y+1)) && (here.cost + cave[y+1][x]) < pathcosts[y+1][x] {
+            let new_pathcost = here.cost + cave[y+1][x];
+            pathcosts[y+1][x] = new_pathcost;
+            let mut i = usize::MAX;
+            for (si, s) in horizon.iter().enumerate() {
+                if s.x == x && s.y == y+1 {
+                    i = si;
+                    break;
+                } 
+            }
+            if i < usize::MAX {
+                horizon[i].cost = new_pathcost;
+            } else {
+                horizon.push(SeenNode { cost: new_pathcost, x: x, y: y+1 });
+            }
+        }
+        donezo.insert((x, y));
+    }
+}
 
-    // walk(&cave, &mut sssp, 0, 0, width-1, height-1);
-
-    return sssp[height-1][width-1];
+fn print_pathcosts(p: &Vec<Vec<usize>>) {
+    for y in 0..p.len() {
+        for x in 0..p.first().unwrap().len() {
+            if p[y][x] == usize::MAX {
+                print!("  *");
+            } else {
+                print!("{:3}", p[y][x]);
+            }
+        }
+        println!();
+    }
 }
