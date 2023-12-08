d878344b50b81c38e314bbad80c97728ae6da6f9
Elora Burns <elora@taaviburns.ca>
2023-12-08 09:55:13 -0500

    Now to migrate this new representation and see if it still solves part 1

--- 2023/day05/src/main.rs
+++ 2023/day05/src/main.rs
@@ -1,4 +1,6 @@
 use std::fs;
+use std::cmp::{max, min};
+//use std::ops::Range;
 
 #[derive(Clone)]
 #[derive(Debug)]
@@ -31,6 +33,7 @@ struct Mappings {
 
 impl Mappings {
     fn map(&self, n: i64) -> i64 {
+        if n < 0 { panic!("Tried to map n={:?} (which is < 0)", n); }
         for m in self.m.iter() {
             if n < m.upto {
                 return n + m.delta;
@@ -44,7 +47,10 @@ impl Mappings {
         smaps.sort();
         let mut dmaps = vec![];
 
-        dmaps.push(Mapping { from: i64::MIN, upto: smaps[0].source, delta: 0 });
+        if smaps[0].source < 0 { panic!("Source out of range!"); }
+        if smaps[0].source > 0 {
+            dmaps.push(Mapping { from: 0, upto: smaps[0].source, delta: 0 });
+        }
 
         let last_smaps = smaps.len() - 1;
         for i in 0..smaps.len() {
@@ -70,26 +76,69 @@ impl Mappings {
         Mappings { m: dmaps }
     }
 
+    fn slice(m: &Mappings, from: i64, upto: i64) -> Vec<Mapping> {
+        // Gives us a 0-based set of Vec<Mapping> as seen from/upto.
+        // Sort of a "projection".
+        let mut out = vec![];
+
+        for ma in m.m.iter() {
+            if ma.upto <= from {
+                continue;
+            }
+            if ma.from >= upto {
+                break;
+            }
+            out.push(Mapping {
+                from: max(from, ma.from) - from,
+                upto: min(upto, ma.upto) - from,
+                delta: ma.delta
+            });
+        }
+        out
+    }
+
     fn merge_mappings(m1: &Mappings, m2: &Mappings) -> Mappings {
         let mut newmaps = vec![];
 
-
+        for ma1 in m1.m.iter() {
+            let projection = Mappings::slice(m2, ma1.from, ma1.upto);
+            for ma2 in projection.iter() {
+                newmaps.push(Mapping {
+                    from: ma1.from + ma2.from,
+                    upto: ma1.from + ma2.upto,
+                    delta: ma1.delta + ma2.delta
+                });
+            }
+        }
 
         Mappings { m: newmaps }
     }
 }
 
 #[test]
+fn test_merge_mappings_simple() {
+    let m1 = Mappings { m: vec![
+        Mapping { from:  0, upto:       10, delta: 0 },
+        Mapping { from: 10, upto:       15, delta: 1 },
+        Mapping { from: 15, upto: i64::MAX, delta: 0 },
+    ]};
+    let m2 = Mappings { m: vec![
+        Mapping { from:  0, upto: i64::MAX, delta: 0 },
+    ]};
+
+    let actual = Mappings::merge_mappings(&m1, &m2);
+    assert_eq!(m1, actual);
+}
+
+#[test]
 fn try_mapping() {
     let ms = Mappings {
         m: vec![
-            Mapping { from: i64::MIN, upto:        0, delta:  0 },
             Mapping { from:        0, upto:        5, delta:  5 },
             Mapping { from:        5, upto:       10, delta: -5 },
             Mapping { from:       10, upto: i64::MAX, delta:  0 },
         ]
     };
-    assert_eq!(-1, ms.map(-1));
     assert_eq!(1, ms.map(6));
     assert_eq!(7, ms.map(2));
     assert_eq!(11, ms.map(11));
@@ -103,7 +152,6 @@ fn test_parsed_to_mappings() {
         ParsedMapping{ source: 16, dest: 17, length: 2}
     ]);
     assert_eq!(Mappings { m: vec![
-        Mapping { from: i64::MIN, upto:        0, delta:   0 },
         Mapping { from:        0, upto:        5, delta:  10 },
         Mapping { from:        5, upto:       10, delta:   0 },
         Mapping { from:       10, upto:       15, delta: -10 },
