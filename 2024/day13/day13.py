3084f22a515506baea770598cf8c18bc0ac5e741
Elora Burns <elora@taaviburns.ca>
2024-12-14 23:47:10 -0500

    WIP 2024-12-13. Feels like another 'n busses showing up at the same time' problem

--- 2024/day13/day13.py
+++ 2024/day13/day13.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 with open("testa.txt") as f:
     testa_txt = f.read()
@@ -24,10 +25,13 @@ class Pair:
     def __add__(self, other):
         return Pair(self.x + other.x, self.y + other.y)
 
-    def __mul__(self, mult):
+    def __sub__(self, other):
+        return Pair(self.x - other.x, self.y - other.y)
+
+    def __rmul__(self, mult):
         return Pair(self.x * mult, self.y * mult)
 
-    def __div__(self, other):
+    def __floordiv__(self, other):
         return min(self.x // other.x, self.y // other.y)
 
 class ClawGame:
@@ -52,11 +56,46 @@ def parse(raw):
         for game in raw.strip().split("\n\n")
     ]
 
+def parse_b(raw):
+    games = []
+    for rawgame in raw.strip().split("\n\n"):
+        game = ClawGame(rawgame)
+        game.prize = game.prize + Pair(10000000000000, 10000000000000)
+        games.append(game)
+    return games
+
+def gcd(a, b):
+    while b:
+        t = b
+        b = a % b
+        a = t
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
 def solve_a_game(game):
-    for a in range(101):
-        for b in range(100, -1, -1):
-            if game.a * a + game.b * b == game.prize:
-                return a*3 + b
+    print(game)
+    a_count = 0
+    b_count = game.prize // game.b
+    agg_lcm = lcm(lcm(game.a.x, game.b.x), lcm(game.a.y, game.b.y))
+    print(f"{agg_lcm=}")
+    for x in range(agg_lcm):
+        if b_count < 0:
+            break
+        current_location = a_count * game.a + b_count * game.b
+        #print(f"  {a_count=} {b_count=} {current_location=} difference={game.prize - current_location}")
+        if current_location == game.prize:
+            return a_count*3 + b_count
+        elif current_location > game.prize:
+            b_count -= 1
+        else:
+            a_count += 1
+    ##for a in range(101):
+    ##    for b in range(100, -1, -1):
+    ##        if game.a * a + game.b * b == game.prize:
+    ##            return a*3 + b
+    #print(".", end="")
     return 0
 
 def part_a(games):
@@ -66,3 +105,16 @@ print("PART A")
 print("TEST EXPECTS 480")
 print(f"TEST RETURNS {part_a(parse(testa_txt))}")
 print(f"ACTUAL RETURNS {part_a(parse(input_txt))}")
+
+bgames = parse_b(testa_txt)
+print("DEBUGGING")
+print(f"\nRunning the first game {bgames[0]}")
+print(f"Solving returns {part_a(bgames[:1])}")
+print(f"\nRunning the second game {bgames[1]}")
+print(f"Solving returns {part_a(bgames[1:2])}")
+
+
+#print("PART B")
+#print("TEST EXPECTS ???")
+#print(f"TEST RETURNS {part_a(bgames)}")
+#print(f"ACTUAL RETURNS {part_a(parse_b(input_txt))}")
